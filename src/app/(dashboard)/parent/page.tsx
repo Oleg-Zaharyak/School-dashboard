99f9705d6fed9dc0c75ea15c969cad0dfca82da3
ozakhariak <ozakhariak@ozakhariak.local>
2024-09-25 17:22:26 +0300

    Added single page for student and teacher

--- src/app/(dashboard)/parent/page.tsx
+++ src/app/(dashboard)/parent/page.tsx
@@ -5,7 +5,7 @@ const ParentPage = () => {
   return (
     <div className="p-4 flex gap-4 flex-col xl:flex-row flex-1">
       {/* LEFT */}
-      <div className="w-full xl:w-2/3">
+      <div className="w-full xl:w-2/3  min-h-[760px]">
         <div className="h-full bg-white p-4 rounded-md">
           <h1 className="text-xl font-semibold">Schedule (John Doe)</h1>
           <BigCalendar />
--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -6,7 +6,7 @@ const StudentPage = () => {
   return (
     <div className="p-4 flex gap-4 flex-col xl:flex-row">
       {/* LEFT */}
-      <div className="w-full xl:w-2/3">
+      <div className="w-full xl:w-2/3  min-h-[760px]">
         <div className="h-full bg-white p-4 rounded-md">
           <h1 className="text-xl font-semibold">Schedule (4A)</h1>
           <BigCalendar />
